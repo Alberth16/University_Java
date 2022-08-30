1ecce86dfa24ea8bae4139be86764d89d5ccb5cd
StormLord89 <goregrindcore95@gmail.com>
2022-08-29 18:52:46 -0600

    add enums: subject type and management type

---
+++ src/main/java/org/example/utils/EManagementType.java
@@ -0,0 +1,9 @@
+package org.example.utils;
+
+public enum EManagementType {
+    Get,
+    GetById,
+    Update,
+    Remove,
+    Save
+}
---
+++ src/main/java/org/example/utils/ESubjectType.java
@@ -0,0 +1,11 @@
+package org.example.utils;
+
+public enum ESubjectType {
+    Maths,
+    English,
+    Literature,
+    Chemistry,
+    History,
+    Physics,
+    Geography
+}
