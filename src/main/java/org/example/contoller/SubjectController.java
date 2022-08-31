a34552d3ad1ea8abce577e1ab4ecdf8232d6e3cb
Stormlord89 <goregrindcore95@gmail.com>
2022-08-31 15:53:50 -0600

    add function to get grade by subject id

--- src/main/java/org/example/contoller/SubjectController.java
+++ src/main/java/org/example/contoller/SubjectController.java
@@ -1,21 +1,29 @@
 package org.example.contoller;
 
 import org.example.model.Student;
+import org.example.model.Subject;
 import org.example.utils.Data;
 
 public class SubjectController {
 
-    public void getGradesBySubjectId(int subjectId){
-// validar que el id este dentro del rando del list de matrias (dentro del rango del size)
-            System.out.println( this.getAverageGrades(subjectId));
-        System.out.println( this.getAverageGrades(subjectId) / Data.students.size());
+    public void getGradesBySubjectId(int subjectId) {
+        if (subjectId <= Data.students.get(0).getSubjects().size() && subjectId > 0) {
+            System.out.println("-------------------------------------------------------------------------------------------------------------------");
+            System.out.printf("%5s %25s %8s %5s%n", "ID", "SUBJECT", "ID STUDENT", "GRADE");
+            System.out.println("-------------------------------------------------------------------------------------------------------------------");
+            System.out.println();
+            /*Data.students.forEach((st) ->{
+                Subject sj = st.getSubjects().get(subjectId - 1);
+                System.out.printf("",subjectId,sj.getName());
+            });*/
 
 
+        }
     }
 
-    private float getAverageGrades(int subjectId){
+    private float getAverageGrades(int subjectId) {
         float gradesSum = 0;
-        for(Student st : Data.students){
+        for (Student st : Data.students) {
             gradesSum += st.getSubjects().get(subjectId).getGrade();
         }
         return gradesSum;
